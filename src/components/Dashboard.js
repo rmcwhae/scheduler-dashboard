e1d5e5e0b07afa3b644dd804200db2006a6bd72f
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-11 10:59:10 -0600

    Adds live-updating via sockets

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Loading from './Loading';
 import Panel from './Panel';
 import { getTotalInterviews, getLeastPopularTimeSlot, getMostPopularDay, getInterviewsPerDay } from 'helpers/selectors';
+import { setInterview } from "helpers/reducers";
 
 const data = [
 	{
@@ -63,12 +64,27 @@ class Dashboard extends Component {
 				interviewers: interviewers.data
 			});
 		});
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
 	}
 
 	componentDidUpdate(previousProps, previousState) {
 		if (previousState.focused !== this.state.focused) {
 			localStorage.setItem('focused', JSON.stringify(this.state.focused));
 		}
+	}
+
+	componentWillUnmount() {
+		this.socket.close();
 	}
 
 	render() {
@@ -87,7 +103,7 @@ class Dashboard extends Component {
 					key={panel.id}
 					id={panel.id}
 					label={panel.label}
-          value={panel.getValue(this.state)}
+					value={panel.getValue(this.state)}
 					onSelect={(event) => this.selectPanel(panel.id)}
 				/>
 			));
