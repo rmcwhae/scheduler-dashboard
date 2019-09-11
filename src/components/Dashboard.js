fffa907c91e2b7b7812f23a47550c06619aabf97
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-11 10:52:46 -0600

    Connects Dashboard to PSQL db

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,41 +1,74 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
+import axios from 'axios';
+
 import Loading from './Loading';
 import Panel from './Panel';
+import { getTotalInterviews, getLeastPopularTimeSlot, getMostPopularDay, getInterviewsPerDay } from 'helpers/selectors';
 
 const data = [
 	{
 		id: 1,
 		label: 'Total Interviews',
-		value: 6
+		getValue: getTotalInterviews
 	},
 	{
 		id: 2,
 		label: 'Least Popular Time Slot',
-		value: '1pm'
+		getValue: getLeastPopularTimeSlot
 	},
 	{
 		id: 3,
 		label: 'Most Popular Day',
-		value: 'Wednesday'
+		getValue: getMostPopularDay
 	},
 	{
 		id: 4,
 		label: 'Interviews Per Day',
-		value: '2.3'
+		getValue: getInterviewsPerDay
 	}
 ];
 
 class Dashboard extends Component {
 	state = {
-		loading: false,
-		focused: null
+		loading: true,
+		focused: null,
+		days: [],
+		appointments: {},
+		interviewers: {}
 	};
 
 	selectPanel(id) {
 		this.setState((previousState) => ({
 			focused: previousState.focused !== null ? null : id
 		}));
+	}
+
+	componentDidMount() {
+		const focused = JSON.parse(localStorage.getItem('focused'));
+
+		if (focused) {
+			this.setState({ focused });
+		}
+
+		Promise.all([
+			axios.get('/api/days'),
+			axios.get('/api/appointments'),
+			axios.get('/api/interviewers')
+		]).then(([ days, appointments, interviewers ]) => {
+			this.setState({
+				loading: false,
+				days: days.data,
+				appointments: appointments.data,
+				interviewers: interviewers.data
+			});
+		});
+	}
+
+	componentDidUpdate(previousProps, previousState) {
+		if (previousState.focused !== this.state.focused) {
+			localStorage.setItem('focused', JSON.stringify(this.state.focused));
+		}
 	}
 
 	render() {
@@ -54,7 +87,7 @@ class Dashboard extends Component {
 					key={panel.id}
 					id={panel.id}
 					label={panel.label}
-					value={panel.value}
+          value={panel.getValue(this.state)}
 					onSelect={(event) => this.selectPanel(panel.id)}
 				/>
 			));
